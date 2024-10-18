01db5d5c7d8f1a47193635b279cda20adeb531ed
Ilsur <ilsur.ibr@gmail.com>
2024-10-19 01:07:45 +0500

    Задача про строки из chekio

--- main.py
+++ main.py
@@ -1,16 +1,8 @@
-my_dict = {'Владимир':1990, 'Василий':1995, 'Михаил':1989}
+s = "greetings, friends."
 
-print('Dict:', my_dict)
-print('Value:', my_dict.get('Владимир'))
-print('Value:', my_dict.get('Антон'))
-my_dict.update({'Антон':2000, 'Елена':2005})
-print('Deleted value:', my_dict.pop('Владимир'))
-print('Dict:', my_dict)
+if s[0].islower():
+    s = s[0].upper() + s[1:]
+if s[-1] != '.':
+    s = s + '.'
 
-my_set = {1, 1, 2, 2, True, False, False, 'a', 'b', 'a'}
-
-print('Set:', my_set)
-my_set.add(4)
-my_set.add('string')
-my_set.discard(False)
-print('Set modified:', my_set)
+print(s)
