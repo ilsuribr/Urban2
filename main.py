24c534b3f728a2ff0c1c8d2e30187a8576405dd3
Ilsur <ilsur.ibr@gmail.com>
2024-10-21 21:30:55 +0500

    bird-language

--- main.py
+++ main.py
@@ -1,20 +1,25 @@
-from itertools import zip_longest
-
-def fuzzy_string_match(str1: str, str2: str, threshold: int) -> bool:
+def translation(text: str) -> str:
     # your code here
-    return sum(map(lambda x: x[0] != x[1], zip_longest(str1, str2))) <= threshold
+    list_text = list(text)
+
+    for i in range(len(list_text)):
+        if list_text[i] not in "aeiouy" and list_text[i] not in ('', ' '):
+            list_text[i + 1] = ''
+        elif list_text[i] in "aeiouy" and list_text[i] != '':
+            list_text[i + 1] = ''
+            list_text[i + 2] = ''
+
+    return ''.join(list_text)
 
 
 print("Example:")
-print(fuzzy_string_match("apple", "appel", 2))
+print(translation("hieeelalaooo"))
 
 # These "asserts" are used for self-checking
-assert fuzzy_string_match("apple", "appel", 2) == True
-assert fuzzy_string_match("apple", "bpple", 1) == True
-assert fuzzy_string_match("apple", "bpple", 0) == False
-assert fuzzy_string_match("apple", "apples", 1) == True
-assert fuzzy_string_match("apple", "bpples", 2) == True
-assert fuzzy_string_match("apple", "apxle", 1) == True
-assert fuzzy_string_match("apple", "pxxli", 3) == False
+assert translation("hieeelalaooo") == "hello"
+assert translation("hoooowe yyyooouuu duoooiiine") == "how you doin"
+assert translation("aaa bo cy da eee fe") == "a b c d e f"
+assert translation("sooooso aaaaaaaaa") == "sos aaa"
 
 print("The mission is done! Click 'Check Solution' to earn rewards!")
+
