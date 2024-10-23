d6714b2dbf6ee6ece9e6d82ceab8191bafbf92bd
Ilsur <ilsur.ibr@gmail.com>
2024-10-23 18:14:28 +0500

    init

---
+++ module_2_3.py
@@ -0,0 +1,9 @@
+my_list = [42, 69, 322, 13, 0, 99, 9, 8, 7, 5]
+
+i = 0
+while i < len(my_list):
+    if my_list[i] > 0:
+        print(my_list[i])
+    elif my_list[i] < 0:
+        break
+    i += 1
