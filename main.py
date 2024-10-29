9604e7da6865551f4ceda4c35ae938cd201634b0
Ilsur <ilsur.ibr@gmail.com>
2024-10-30 00:34:31 +0500

    quadratic_roots

--- main.py
+++ main.py
@@ -1,29 +1,31 @@
-def is_leap_year(year: int) -> bool:
+from collections.abc import Iterable
+from typing import Union
+
+
+def quadratic_roots(a: int, b: int, c: int) -> Iterable[Union[int | float] | str]:
     # your code here
-    if year % 400 == 0:
-        return True
-    elif year % 100 == 0:
-        return False
-    elif (year % 4 == 0):
-        return True
+    x1 = (-1 * b + (b ** 2 - 4 * a * c) ** 0.5) / (2 * a)
+    x2 = (-1 * b - (b ** 2 - 4 * a * c) ** 0.5) / (2 * a)
+
+    if type(x1) == complex or type(x2) == complex:
+        return ["No real roots"]
     else:
-        return False
+        return sorted([x1, x2], reverse=True)
 
 
 print("Example:")
-print(is_leap_year(1891))
+print(list(quadratic_roots(1, 2, 3)))
 
 # These "asserts" are used for self-checking
-assert is_leap_year(2000) == True
-assert is_leap_year(1900) == False
-assert is_leap_year(2004) == True
-assert is_leap_year(2100) == False
-assert is_leap_year(2020) == True
-assert is_leap_year(2021) == False
-assert is_leap_year(1600) == True
-assert is_leap_year(1700) == False
-assert is_leap_year(1800) == False
-assert is_leap_year(2400) == True
+assert list(quadratic_roots(1, -3, 2)) == [2, 1]
+assert list(quadratic_roots(1, 0, -1)) == [1, -1]
+assert list(quadratic_roots(1, 2, 1)) == [-1, -1]
+assert list(quadratic_roots(1, 0, 1)) == ["No real roots"]
+assert list(quadratic_roots(1, 4, 4)) == [-2, -2]
+assert list(quadratic_roots(1, -5, 6)) == [3, 2]
+assert list(quadratic_roots(1, -6, 9)) == [3, 3]
+assert list(quadratic_roots(2, 2, 1)) == ["No real roots"]
+assert list(quadratic_roots(2, -7, 6)) == [2, 1.5]
+assert list(quadratic_roots(2, -3, 1)) == [1, 0.5]
 
 print("The mission is done! Click 'Check Solution' to earn rewards!")
-
