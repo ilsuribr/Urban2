c2bc78e48745aa79741efe91c9ce734bac8193ed
Ilsur <ilsur.ibr@gmail.com>
2024-10-31 20:49:09 +0500

    init

---
+++ module_3_3.py
@@ -0,0 +1,19 @@
+def print_params(a=1, b='строка', c=True):
+    print(a, b, c)
+
+
+print_params()
+print_params(2)
+print_params(2, 3)
+print_params(2, 3, 'Привет')
+print_params(b = 25)
+print_params(c = [1, 2, 3])
+
+values_list = [1, True, 'Строка']
+values_dict = {'a': 1, 'b': 'строка', 'c': True}
+
+print_params(*values_list)
+print_params(**values_dict)
+
+values_list_2 = [1, 'Строка']
+print_params(*values_list_2, 42)
