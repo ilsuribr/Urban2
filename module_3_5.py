2fb0922c18aff441776678fd30d7d46ae59198a1
Ilsur <ilsur.ibr@gmail.com>
2024-11-03 01:02:01 +0500

    init

--- module_3_5.py
+++ module_3_5.py
@@ -1,6 +1,6 @@
 def get_multiplied_digits(number):
     if len(str(number)) == 1:
-        return int(str(number))
+        return number
     else:
         return int(str(number)[0]) * get_multiplied_digits(int(str(number)[1:]))
 
