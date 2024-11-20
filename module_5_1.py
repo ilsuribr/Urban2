821fa1b0804e0f5d562c7904e728b0270dd582ad
Ilsur <ilsur.ibr@gmail.com>
2024-11-21 03:49:21 +0500

    init

---
+++ module_5_1.py
@@ -0,0 +1,15 @@
+class House:
+    def __init__(self, name, number_of_floors):
+        self.name = name
+        self.number_of_floors = number_of_floors
+
+    def go_to(self, new_floor):
+        if new_floor > self.number_of_floors or new_floor < 1:
+            print('Такого этажа не существует')
+        else:
+            for i in range(1, new_floor + 1):
+                print(i)
+
+
+house = House('Акварель', 20)
+house.go_to(10)
