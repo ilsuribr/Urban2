e92e4b96763050055f0318ac2652b1b8949b25b5
Ilsur <ilsur.ibr@gmail.com>
2024-11-21 17:32:42 +0500

    init

---
+++ module_5_2.py
@@ -0,0 +1,29 @@
+class House:
+    def __init__(self, name, number_of_floors):
+        self.name = name
+        self.number_of_floors = number_of_floors
+
+    def go_to(self, new_floor):
+        if new_floor > self.number_of_floors or new_floor < 1:
+            print('Такого этажа не существует')
+        else:
+            for i in range(1, new_floor + 1):
+                print(i)
+
+    def __len__(self):
+        return self.number_of_floors
+
+    def __str__(self):
+        return f'Название: {self.name}, кол-во этажей: {self.number_of_floors}'
+
+
+house1 = House('Акварель', 20)
+house2 = House('Элитный', 30)
+
+# __str__
+print(house1)
+print(house2)
+
+# __len__
+print(len(house1))
+print(len(house2))
